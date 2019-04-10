480c36c6e36beab0ca2e85028d444993686f2302
haoyuanjun <haoyuanjun@xywy.com>
2019-04-10 16:45:36 +0800

    edit

--- Sum.php
+++ Sum.php
@@ -82,11 +82,103 @@ class Solution {
         }
         return $result;
     }
+
+    /**
+     * no repeat substring
+     * @param String $s
+     * @return Integer
+     */
+    function getLongestSubstring($s) {
+        $result = $lengthArr = array();
+        $i = 0;//substring-counts
+        $charArr = str_split($s);//string to array
+        $self = $charArr;//single-char is also no-repeat-substring
+        while($charArr){
+          foreach($charArr as $start=>$char){
+              if(!isset($result[$i])){
+                  $result[$i] = array();
+                  $j = 0;//every-substring-length
+              }
+              if(in_array($char,$result[$i])){
+                  if($j == 1){
+                    unset($self[$i]);
+                  }
+                  else{
+                    $self[$i] = array($self[$i]);
+                  }
+                  $lengthArr[$i] = $j;//count($result[$i]);
+                  $i++;
+                  $j = 0;
+                  unset($charArr[0]);//from left to right
+                  $charArr = array_values($charArr);
+                  break;
+              }
+              $result[$i][] = $char;
+              $j++;
+
+              if($start == count($charArr)-1){//search until the last char
+                  if($j == 1){
+                    unset($self[$i]);
+                  }
+                  else{
+                    $self[$i] = array($self[$i]);
+                  }
+
+                  $i++;
+                  $j = 0;
+                  unset($charArr[0]);//from left to right
+                  $charArr = array_values($charArr);
+              }
+          }
+        }
+        //var_dump($result);
+        $lengthArr[$i] = $j;//count($result[$i]);
+
+        //all no-repeat-substring
+        $result = array_merge($result,$self);//var_dump($result);
+        foreach($self as $k=>$v){
+          $lengthArr[$i] = 1;
+          $i++;
+        }
+
+        $max = max($lengthArr);
+        $key = array_search($max,$lengthArr);
+        $ret['length'] = $max;
+        $ret['string'] = implode('',$result[$key]);
+        return $ret;
+    }
+
+    /**
+     * @param Integer[] $nums1
+     * @param Integer[] $nums2
+     * @return Float
+     */
+    function findMedianSortedArrays($nums1, $nums2) {
+        $num = array_values(array_merge($nums1,$nums2));
+        sort($num);
+        $count = count($num);
+        $is_ord = $count%2;
+        if($is_ord == 0){//偶数
+            $key = $count/2;
+            return round(($num[$key-1]+$num[$key])/2,1);
+        }
+        else{//奇数
+            $key = floor($count/2);
+            return $num[$key];
+        }
+    }
 }
 
 $nums = array(2, 9,'3'=> 2,0, 'a'=>1, 8, 7, 11, 15);
 $target = 9;
 $obj = new Solution;
-$ret = $obj->twoSum2($nums,$target);
+//$ret = $obj->twoSum2($nums,$target);
 
+$s = 'pwwkew';var_dump($s);
+$ret = $obj->getLongestSubstring($s);
+var_dump($ret);
+
+$nums1 = [1, 2];
+$nums2 = [3, 4];
+$ret = $obj->findMedianSortedArrays($nums1, $nums2);
 var_dump($ret);
